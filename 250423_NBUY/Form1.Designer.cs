499aef6cebc071488954dd05a5b677232c9e7260
yilma <yilma@DESKTOP-U1LH654>
2023-04-25 16:45:22 +0300

    Add project files.

---
+++ 250423_NBUY/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace _250423_NBUY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstKategori = new System.Windows.Forms.ListBox();
+            this.LstUrunler = new System.Windows.Forms.ListBox();
+            this.LstTedarikci = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // LstKategori
+            // 
+            this.LstKategori.FormattingEnabled = true;
+            this.LstKategori.Location = new System.Drawing.Point(236, 37);
+            this.LstKategori.Name = "LstKategori";
+            this.LstKategori.Size = new System.Drawing.Size(240, 290);
+            this.LstKategori.TabIndex = 0;
+            // 
+            // LstUrunler
+            // 
+            this.LstUrunler.FormattingEnabled = true;
+            this.LstUrunler.Location = new System.Drawing.Point(12, 37);
+            this.LstUrunler.Name = "LstUrunler";
+            this.LstUrunler.Size = new System.Drawing.Size(197, 290);
+            this.LstUrunler.TabIndex = 1;
+            // 
+            // LstTedarikci
+            // 
+            this.LstTedarikci.FormattingEnabled = true;
+            this.LstTedarikci.Location = new System.Drawing.Point(503, 37);
+            this.LstTedarikci.Name = "LstTedarikci";
+            this.LstTedarikci.Size = new System.Drawing.Size(264, 290);
+            this.LstTedarikci.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LstTedarikci);
+            this.Controls.Add(this.LstUrunler);
+            this.Controls.Add(this.LstKategori);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstKategori;
+        private System.Windows.Forms.ListBox LstUrunler;
+        private System.Windows.Forms.ListBox LstTedarikci;
+    }
+}
+
---
+++ 250423_NBUY/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _250423_NBUY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            //server=localhost veya . konulursa pcdeki servera baglanır.
+            baglanti.ConnectionString = "Server=localhost;Database=Northwind;User=sa;Pwd=123";
+            SqlCommand cmd = new SqlCommand();
+            //yeni bir command nesnesi oluşturuluyor. içerisine işlem yazılacağı için
+            cmd.CommandText = "Select * from Urunler";
+            cmd.Connection = baglanti;
+            baglanti.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string adi = reader["UrunAdi"].ToString();
+                string fiyat = reader["BirimFiyati"].ToString();
+                string stok = reader["HedefStokDuzeyi"].ToString();
+                LstUrunler.Items.Add(string.Format("{0}-{1}-{2}", adi, fiyat, stok));
+
+            }
+            baglanti.Close();
+            KategoriListele();
+            TedarikciListele();
+        }
+        private void KategoriListele()
+        {
+            SqlCommand cmd = new SqlCommand("Select * from Kategoriler", baglanti);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string KategoriID = reader["KategoriID"].ToString();
+                string KategoriAdi = reader["KategoriAdi"].ToString();
+                string tanimi = reader["Tanimi"].ToString();
+                LstKategori.Items.Add(string.Format("{0}-{1}-{2}", KategoriID, KategoriAdi, tanimi));
+            }
+            baglanti.Close();
+        }
+        private void TedarikciListele()
+        {
+            SqlCommand cmd = new SqlCommand("Select * from Tedarikciler", baglanti);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string TedarikciID = reader["TedarikciID"].ToString();
+                string SirketAdi = reader["SirketAdi"].ToString();
+                string Sehir = reader["Sehir"].ToString();
+                LstTedarikci.Items.Add(string.Format("{0}-{1}-{2}", TedarikciID, SirketAdi, Sehir));
+            }
+            baglanti.Close();
+        }
+    }
+}
